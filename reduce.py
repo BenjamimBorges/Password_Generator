94781afc78dee8631b23c9f0f382c9b071ff591d
Benjamim Borges <borges.benjamim@gmail.com>
2023-12-29 21:42:53 -0300

    subindo o gerador

---
+++ reduce.py
@@ -0,0 +1,10 @@
+import string as st
+import numpy as np
+from tkinter import *
+
+letras = st.ascii_letters
+numeros = st.digits
+especial = st.punctuation
+algarismo = letras+numeros+especial
+senha = np.random.choice(list(algarismo),10) 
+print(''.join(senha))
